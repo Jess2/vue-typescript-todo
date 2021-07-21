51d4f21e5f7c8777a60778a7185c644e0d9a98cd
JESS2 <jess2.developer@gmail.com>
2021-07-21 16:45:29 +0900

    todo list item 정렬 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -28,11 +28,11 @@ import TodoListItem from "@/components/TodoListItem.vue";
 
 const STORAGE_KEY = "vue-todo-ts-v1";
 const storage = {
-  save(todoItems: any[]) {
+  save(todoItems: Todo[]) {
     const parsed = JSON.stringify(todoItems);
     localStorage.setItem(STORAGE_KEY, parsed);
   },
-  fetch() {
+  fetch(): Todo[] {
     const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
     return JSON.parse(todoItems);
   },
@@ -72,7 +72,15 @@ export default Vue.extend({
       this.todoText = "";
     },
     fetchTodoItems() {
-      this.todoItems = storage.fetch();
+      this.todoItems = storage.fetch().sort((a, b) => {
+        if (a.title < b.title) {
+          return -1;
+        }
+        if (a.title > b.title) {
+          return 1;
+        }
+        return 0;
+      });
     },
     toggleTodoItem(todoItem: Todo, index: number) {
       this.todoItems.splice(index, 1, {
