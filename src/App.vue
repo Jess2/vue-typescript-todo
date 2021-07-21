f704c3a8cc4d837cfadfa1a944e5a9f05d6424a0
JESS2 <jess2.developer@gmail.com>
2021-07-21 13:32:38 +0900

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>Vue Todo with TypeScript</h1>
+    <TodoInput />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoInput from "@/components/TodoInput.vue";
 
 export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: { TodoInput },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" />
+    <button @click="addTodo" type="button">Add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "TodoInput",
+  methods: {
+    addTodo() {
+      console.log("add");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
