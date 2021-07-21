048daf53f130fe95d8f6011acb2d581550c41451
JESS2 <jess2.developer@gmail.com>
2021-07-21 15:32:33 +0900

    todo list item 추가 및 조회 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div>
-    <h1>Vue Todo with TypeScript</h1>
-    <TodoInput :item="todoText" @input="updateTodoText" @add="addTodoItem" />
+    <header>
+      <h1>Vue Todo with TypeScript</h1>
+    </header>
+    <main>
+      <TodoInput :item="todoText" @input="updateTodoText" @add="addTodoItem" />
+      <div>
+        <ul>
+          <TodoListItem
+            v-for="(todoItem, index) in todoItems"
+            :key="index"
+            :todoItem="todoItem"
+          />
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TodoInput from "@/components/TodoInput.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItems: any[]) {
+    const parsed = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+  fetch() {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    return JSON.parse(todoItems);
+  },
+};
 
 export default Vue.extend({
   data() {
     return {
       todoText: "",
+      todoItems: [] as any[],
     };
+  },
+  created() {
+    this.fetchTodoItems();
   },
   methods: {
     updateTodoText(value: string) {
@@ -21,14 +51,18 @@ export default Vue.extend({
     },
     addTodoItem() {
       const value = this.todoText;
-      localStorage.setItem(value, value);
+      this.todoItems.push(value);
+      storage.save(this.todoItems);
       this.initTodoText();
     },
     initTodoText() {
       this.todoText = "";
     },
+    fetchTodoItems() {
+      this.todoItems = storage.fetch();
+    },
   },
-  components: { TodoInput },
+  components: { TodoInput, TodoListItem },
 });
 </script>
 
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>{{ todoItem }}</li>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    todoItem: {
+      type: String,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
